7322ec266f9437d306f454532726da20efedcf70
yusho-nakawaki <ygoriko@gmail.com>
2022-08-01 10:30:11 +0900

    composeの設定

---
+++ app/src/main/java/com/example/bottomsheetbehavior/ProfileEditPage.kt
@@ -0,0 +1,24 @@
+package com.example.bottomsheetbehavior
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileEditPage() {
+//    Column() {
+        Spacer(modifier = Modifier.height(80.dp))
+        Text(text = "aaa")
+//    }
+}
+
+@Preview
+@Composable
+fun ProfileEditPagePreview() {
+    ProfileEditPagePreview()
+}
--- app/src/main/java/com/example/bottomsheetbehavior/SecondFragment.kt
+++ app/src/main/java/com/example/bottomsheetbehavior/SecondFragment.kt
@@ -35,6 +35,10 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        binding.rankComposeView.setContent {
+            ProfileEditPage()
+        }
     }
 
     override fun onDestroyView() {
