21fc24c443db6852ea45493326fac79c6d9df457
yusho-nakawaki <ygoriko@gmail.com>
2022-08-03 21:16:27 +0900

    expandableTextViewの実装

--- app/src/main/java/com/example/bottomsheetbehavior/FirstFragment.kt
+++ app/src/main/java/com/example/bottomsheetbehavior/FirstFragment.kt
@@ -36,9 +36,9 @@ class FirstFragment : Fragment() {
         val behavior = BottomSheetBehavior.from(binding.bottomSheet)
         behavior.state = BottomSheetBehavior.STATE_HIDDEN
 
-        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 
             if (behavior.state == BottomSheetBehavior.STATE_EXPANDED) {
                 behavior.state = BottomSheetBehavior.STATE_HIDDEN
